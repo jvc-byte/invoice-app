5ee93d4ed7f3027f36f4ea22597c194b28b179ff
jvc-byte <jvc8463@gmail.com>
2025-01-27 15:48:04 +0100

    implemented streamming: showing loading before delivering actual content

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,13 @@
+
+import { lusitana } from '@/app/ui/fonts';
+
+export default async function Page() {
+
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Loading...
+            </h1>
+        </main>
+    );
+}
