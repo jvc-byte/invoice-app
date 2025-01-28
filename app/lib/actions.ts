2801d7ea913a76b40e881ffa84048b6344e4633a
jvc-byte <jvc8463@gmail.com>
2025-01-28 02:18:18 +0100

    Implemented delete invoice. Done Mutation of data.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -33,7 +33,7 @@ export async function createInvoice(formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
-
+/** Update Invoice */
 export async function updateInvoice(id: string, formData: FormData) {
     const { customerId, amount, status } = UpdateInvoice.parse({
       customerId: formData.get('customerId'),
@@ -51,4 +51,9 @@ export async function updateInvoice(id: string, formData: FormData) {
    
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+  }
+/** Delete Invoice */
+  export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
   }
